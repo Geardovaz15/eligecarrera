ce2c4da4383d3d77c6e58317ff3706881329d605
Geardovaz15 <163466586+Geardovaz15@users.noreply.github.com>
2024-05-19 22:23:39 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+///funciones para referir a links
+function iniciar(){
+  location.href="preguntas1.html";
+}
+function home(){
+  location.href="index.html";
+}
+function cuceiF(){
+  location.href="https://www.facebook.com/udegcucei/?locale=es_LA";
+}
+function cuceiW(){
+  location.href="http://www.cucei.udg.mx/";
+}
+function cuceaF(){
+  location.href="https://www.facebook.com/cucea.oficial/?locale=es_LA";
+}
+function cuceaW(){
+  location.href="https://www.cucea.udg.mx/";
+}
+function cucadF(){
+  location.href="https://es-la.facebook.com/CUAAD/";
+}
+function cucadW(){
+  location.href="https://cuaad.udg.mx/";
+}
+function cucsF(){
+  location.href="https://www.facebook.com/CUCSUdeG/?locale=es_LA";
+}
+function cucsW(){
+  location.href="https://www.cucs.udg.mx/";
+}
+function cucshW(){
+  location.href="http://www.cucsh.udg.mx/";
+}
+function udgF(){
+  location.href="https://www.facebook.com/udg.mx/?locale=es_LA";
+}
+function udgW(){
+  location.href="https://www.udg.mx/es";
+}
+function CucshF(){
+  location.href="https://www.facebook.com/OficialDifundeCUCSH/";
+}
+function CucshW(){
+  location.href="http://www.cucsh.udg.mx/";
+}
+function CuciF(){
+  location.href="https://www.facebook.com/cucienega.oficial/?locale=es_LA";
+}
+function CuciW(){
+  location.href="https://cuci.udg.mx/";
+}
+function CulaF(){
+  location.href="https://www.facebook.com/p/CULagos-100057534186504/?locale=es_LA";
+}
+function CulaW(){
+  location.href="https://www.lagos.udg.mx/";
+}
+function CunorteF(){
+  location.href="https://www.facebook.com/centrouniversitariodelnorte/?locale=es_LA";
+}
+function CunorteW(){
+  location.href="https://www.cunorte.udg.mx/";
+}
+function CucF(){
+  location.href="https://www.facebook.com/CUCostaUDG/?locale=es_LA";
+}
+function CucW(){
+  location.href="http://www.cuc.udg.mx/";
+}
